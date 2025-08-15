c0e7b565cb4d7e3b982be092381c333d929815d7
jordana <jorperes53@gmail.com>
2025-08-15 10:28:54 -0300

    lista de exercicios 1 a

---
+++ ex_1_a.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+  int main(){
+      
+  int A, B, soma;
+  
+  printf("Digite o primeiro numero:");
+  scanf("%d", &A);
+  
+  printf("Digite o segundo numero:");
+  scanf("%d", &B);
+  
+  soma = A + B;
+  
+//   printf(" A soma e: %d\n, soma");
+  printf("\n %d + %d = %d", A, B, soma);
+  
+ return 0;
+    
+}
