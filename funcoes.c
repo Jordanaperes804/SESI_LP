e064ac01c4aca4b44faa58413ba2f3122d6e7c48
Jordana <jorperes53@gmail.com>
2025-09-05 09:24:19 -0300

    funcoes.c

---
+++ funcoes.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void main(){
+    
+printf ("Bom dia sextou \n");
+int A =0;
+int B =0;
+
+printf ("digite o valor de A: \n ");
+scanf ("%d", &A);
+
+printf ("digite o valor de B: \n");
+scanf ("%d", &B);
+
+printf("A soma de A+B = %d", A+B);
+    
+}   
+    
+    
+    
+    
+    
+    
+    
