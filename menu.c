648d3a66a1b7f108d0ace0ad34c5b6ae3e2403d1
Jordana <jorperes53@gmail.com>
2025-09-05 11:27:08 -0300

    aula 05/09

---
+++ menu.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+
+void SOMA(){
+int A = 0;
+int B = 0;
+ 
+ printf("Digite o valor de A: ");
+ scanf("%d", &A);
+ 
+ printf("Digite o valor de B: ");
+ scanf("%d", &B);
+  printf("%d + %d = %d\n", A, B, A+B);
+
+}
+
+void DIVISÃO(){
+
+int A = 0;
+ int B = 0;
+ 
+ printf("Digite o valor de A: ");
+ scanf("%d", &A);
+ 
+ printf("Digite o valor de B: ");
+ scanf("%d", &B);
+  printf("%d / %d = %d\n", A, B, A / B);
+}
+void main(){
+    
+     printf ("SELECIONE UMA OPERAÇÃO MATEMÁTICA"  );
+     printf ("---------------------------------\n");
+     printf ("-            CALCULADORA         \n");
+     printf ("---------------------------------\n");
+     printf ("- [1] SOMA                     - \n");   
+     printf ("- [2] SUBTRAÇÃO                - \n");
+     printf ("- [3] DIVISÃO                  - \n");
+     printf ("- [4] MULTIPLICAÇÃO            - \n");
+     printf ("---------------------------------\n");
+ 
+     int opção = 0;
+     scanf("%d", &opção);
+ 
+     switch(opção){
+         case 1:
+            printf ("SOMA");
+            
+         break;
+        
+        case 2:
+            printf ("SUBTRAÇÃO");
+        break;
+        
+        case 3:
+            printf ("DIVISÃO");
+        break;
+        
+        case 4:
+            printf ("MULTIPLICAÇÃO");
+        break;
+        
+        default:
+            printf ("A OPERAÇÃO É INVÁLIDA");
+    } 
+     
+}
